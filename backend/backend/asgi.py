ad4752402a0ed07e76a135d37c3bbc523bbfdf9c
root <root@srv1125366.hstgr.cloud>
2026-01-04 20:36:43 +0000

    added whitelist

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -2,17 +2,22 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.conf import settings
+from starlette.routing import Mount
+from starlette.staticfiles import StaticFiles
 import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
-
 django_asgi_app = get_asgi_application()
-django_asgi_app = ASGIStaticFilesHandler(django_asgi_app)
 
+# Serve static files via WhiteNoise (already done in settings)
+# Serve media files via ASGI
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": URLRouter([
+        Mount("/media", app=StaticFiles(directory=settings.MEDIA_ROOT), name="media"),
+        Mount("/", app=django_asgi_app),  # Django app handles all other routes
+    ]),
     "websocket": AuthMiddlewareStack(
         URLRouter(core.routing.websocket_urlpatterns)
     ),
